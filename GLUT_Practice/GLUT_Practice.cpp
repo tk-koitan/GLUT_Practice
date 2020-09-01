02a3fe27ae81d2d3cb79a977b4ed57dd6c90dfcd
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-09-01 12:19:53 +0900

    10_3まで進めた

--- GLUT_Practice/GLUT_Practice.cpp
+++ GLUT_Practice/GLUT_Practice.cpp
@@ -1,4 +1,5 @@
-﻿#include <GL/glut.h>
+#include <stdlib.h>
+#include <GLUT/GLUT.h>
 
 GLdouble vertex[][3] = {
   { 0.0, 0.0, 0.0 },
@@ -11,60 +12,131 @@ GLdouble vertex[][3] = {
   { 0.0, 1.0, 1.0 }
 };
 
-int edge[][2] = {
-  { 0, 1 },
-  { 1, 2 },
-  { 2, 3 },
-  { 3, 0 },
-  { 4, 5 },
-  { 5, 6 },
-  { 6, 7 },
-  { 7, 4 },
-  { 0, 4 },
-  { 1, 5 },
-  { 2, 6 },
-  { 3, 7 }
+int face[][4] = {
+  { 0, 1, 2, 3 },
+  { 1, 5, 6, 2 },
+  { 5, 4, 7, 6 },
+  { 4, 0, 3, 7 },
+  { 4, 5, 1, 0 },
+  { 3, 2, 6, 7 }
 };
+
+GLdouble color[][3] = {
+  { 1.0, 0.0, 0.0 },
+  { 0.0, 1.0, 0.0 },
+  { 0.0, 0.0, 1.0 },
+  { 1.0, 1.0, 0.0 },
+  { 1.0, 0.0, 1.0 },
+  { 0.0, 1.0, 1.0 }
+};
+
+void idle(void)
+{
+  glutPostRedisplay();
+}
 
 void display(void)
 {
-	int i;
+  int i;
+  int j;
+  static int r = 0; /* 回転角 */
 
-	glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	/* 図形の描画 */
-	glColor3d(0.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-	for (i = 0; i < 12; ++i) {
-		glVertex3dv(vertex[edge[i][0]]);
-		glVertex3dv(vertex[edge[i][1]]);
-	}
-	glEnd();
+  glLoadIdentity();
 
-	glFlush();
+  /* 視点位置と視線方向 */
+  gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  /* 図形の回転 */
+  glRotated((double)r, 0.0, 1.0, 0.0);
+
+  /* 図形の描画 */
+  glColor3d(0.0, 0.0, 0.0);
+  glBegin(GL_QUADS);
+  for (j = 0; j < 6; ++j) {
+    glColor3dv(color[j]);
+    for (i = 0; i < 4; ++i) {
+      glVertex3dv(vertex[face[j][i]]);
+    }
+  }
+  glEnd();
+
+  glutSwapBuffers();
+
+  /* 一周回ったら回転角を 0 に戻す */
+  if (++r >= 360) r = 0;
 }
 
 void resize(int w, int h)
 {
-	glViewport(0, 0, w, h);
+  glViewport(0, 0, w, h);
 
-	glLoadIdentity();
-	gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
-	gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  /* 透視変換行列の設定 */
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
+
+  /* モデルビュー変換行列の設定 */
+  glMatrixMode(GL_MODELVIEW);
 }
+
+void mouse(int button, int state, int x, int y)
+{
+  switch (button) {
+  case GLUT_LEFT_BUTTON:
+    if (state == GLUT_DOWN) {
+      /* アニメーション開始 */
+      glutIdleFunc(idle);
+    }
+    else {
+      /* アニメーション停止 */
+      glutIdleFunc(0);
+    }
+    break;
+  case GLUT_RIGHT_BUTTON:
+    if (state == GLUT_DOWN) {
+      /* コマ送り (1ステップだけ進める) */
+        glutPostRedisplay();
+    }
+    break;
+  default:
+    break;
+  }
+}
+  
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key) {
+  case 'q':
+  case 'Q':
+  case '\033':  /* '\033' は ESC の ASCII コード */
+    exit(0);
+  default:
+    break;
+  }
+}
+
 
 void init(void)
 {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
 }
 
 int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutReshapeFunc(resize);
+    glutMouseFunc(mouse);
+    glutKeyboardFunc(keyboard);
 	init();
 	glutMainLoop();
 	return 0;
