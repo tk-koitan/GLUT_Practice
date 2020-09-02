3d2e7c7f084dc08fd50446c11b9d20c1c9fd6f2d
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-09-02 15:35:26 +0900

    実験課題1をとりあえず終えた(マウスの制御に難あり)

--- GLUT_Practice/GLUT_Practice.cpp
+++ GLUT_Practice/GLUT_Practice.cpp
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <GLUT/GLUT.h>
+#include <math.h>
+
+#define deg2rad(deg) (((deg)/360)*2*M_PI)
 
 double input_x = 0;
 double input_y = 0;
-int key_state[256];
+int prev_pos_x = 0;
+int prev_pos_y = 0;
+int current_pos_x = 0;
+int current_pos_y = 0;
 
-int key(char k)
-{
-    return key_state[k - 'a'];
-}
 
 void scene(void)
 {
@@ -72,11 +74,17 @@ void display(void)
     static GLfloat lightpos[] = { 3.0, 4.0, 5.0, 1.0 }; /* 光源の位置 */
     
     static double ex = 0.0, ez = 0.0; /* 視点の位置 */
-    static double r = 0.0;            /* 視点の向き */
+    static double rx = 0.0;            /* 視点の向き */
+    static double ry = 0.0;
+    
+    //視点移動
+    rx += (current_pos_x - prev_pos_x) * 0.2;
+    ry += (current_pos_y - prev_pos_y) * 0.2;
     
     //座標移動
-    ex -= input_x * 0.1;
-    ez += input_y * 0.1;
+    ex += (-input_x * cos(deg2rad(rx)) - input_y * sin(deg2rad(rx))) * 0.1;
+    ez += (-input_x * sin(deg2rad(rx)) + input_y * cos(deg2rad(rx))) * 0.1;
+    
     
     /* 画面クリア */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -85,7 +93,8 @@ void display(void)
     glLoadIdentity();
     
     /* 視点の移動 */
-    glRotated(r, 0.0, 1.0, 0.0);
+    glRotated(rx, 0.0, 1.0, 0.0);
+    glRotated(ry, cos(deg2rad(rx)), 0.0, sin(deg2rad(rx)));
     glTranslated(ex, 0.0, ez);
     
     /* 光源の位置を設定 */
@@ -157,7 +166,7 @@ void keyboard_up(unsigned char key, int x, int y)
     {
         input_x = 0;
     }
-
+    
     
     if(key == 's')
     {
@@ -167,6 +176,21 @@ void keyboard_up(unsigned char key, int x, int y)
     {
         input_y = 0;
     }
+}
+
+void mouse(int x, int y)
+{
+    prev_pos_x = current_pos_x;
+    prev_pos_y = current_pos_y;
+    current_pos_x = x;
+    current_pos_y = y;
+}
+
+void mouse_down(int x, int y)
+{
+    int ww = glutGet(GLUT_WINDOW_WIDTH);
+    int wh = glutGet(GLUT_WINDOW_HEIGHT);
+    glutWarpPointer(ww / 2, wh / 2);
 }
 
 void init(void)
@@ -181,6 +205,7 @@ void init(void)
 
 int main(int argc, char *argv[])
 {
+    glutInitWindowSize(640, 480);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow(argv[0]);
@@ -188,6 +213,8 @@ int main(int argc, char *argv[])
     glutReshapeFunc(resize);
     glutKeyboardFunc(keyboard_down);
     glutKeyboardUpFunc(keyboard_up);
+    glutPassiveMotionFunc(mouse);
+    glutMotionFunc(mouse_down);
     glutIdleFunc(display);
     init();
     glutMainLoop();
